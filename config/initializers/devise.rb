701660cfd91064c2265a3533b8203583c368a7cc
Ryan F. Salerno <rfsalerno@gmail.com>
2013-11-26 01:26:04 -0500

    final changes lol final

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -135,7 +135,7 @@ Devise.setup do |config|
 
   # ==> Configuration for :validatable
   # Range for password length. Default is 8..128.
-  config.password_length = 8..128
+  config.password_length = 5..128
 
   # Email regex used to validate email formats. It simply asserts that
   # one (and only one) @ exists in the given string. This is mainly
@@ -145,7 +145,7 @@ Devise.setup do |config|
   # ==> Configuration for :timeoutable
   # The time you want to timeout the user session without activity. After this
   # time the user will be asked for credentials again. Default is 30 minutes.
-  # config.timeout_in = 30.minutes
+   config.timeout_in = 525949.minutes
 
   # If true, expires auth token on session timeout.
   # config.expire_auth_token_on_timeout = false
