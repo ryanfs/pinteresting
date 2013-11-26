97f8ada53105feb3bd64ced9bed6f4b8a8db4317
Ryan F. Salerno <rfsalerno@gmail.com>
2013-11-25 21:57:44 -0500

    added names to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,5 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "http://lorempixel.com/output/cats-q-g-300-300-2.jpg"
+	validates :image, presence: true
 end
