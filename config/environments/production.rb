3ea49569dda857e74846a56ada2289b913c3ce49
Ryan F. Salerno <rfsalerno@gmail.com>
2013-11-25 19:12:48 -0500

    fixed production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ config.action_mailer.default_url_options = { :host => 'fake-pinterest-rfs.heroku
 config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
+    :bucket => ENV['S3_BUCKET'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
